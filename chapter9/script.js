3f24858c290b89e0a0db4aaee5e82150680e98a3
RChaffino <67568390+RChaffino@users.noreply.github.com>
2020-07-14 16:21:36 -0700

    Create script.js

---
+++ chapter9/script.js
@@ -0,0 +1,28 @@
+/*    JavaScript 6th Edition
+ *    Chapter 9
+ *    Chapter case
+
+ *    Eating Well in Season 
+ *    Author: Rich Chaffino
+ *    Date:   14 July 2020
+
+ *    Filename: script.js
+ */
+
+"use strict";
+
+function clearCookies() {
+	var cookieString = document.cookie;
+	var cookieArray = cookieString.split("; ");
+	var expiresDate = newDate();
+	expiresDate.setDate(expiresDate.getDate() - 7);
+	for (var i = 0; i < cookieArray.length; i++) {
+		document.cookie = cookieArray[i] + "; expires=" + expiresDate.toUTCString();
+	}
+}
+
+if (window.addEventListener) {
+	window.addEventListener("load",clearCookies, false);
+} else if (window.attachEvent) {
+	window.attachEvent("onload", clearCookies);
+}
