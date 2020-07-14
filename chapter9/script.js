dba46ee569652d17779b914a4e9f8ce371e11f7c
RChaffino <67568390+RChaffino@users.noreply.github.com>
2020-07-14 16:32:20 -0700

    https font

--- chapter9/script.js
+++ chapter9/script.js
@@ -14,7 +14,7 @@
 function clearCookies() {
 	var cookieString = document.cookie;
 	var cookieArray = cookieString.split("; ");
-	var expiresDate = newDate();
+	var expiresDate = new Date();
 	expiresDate.setDate(expiresDate.getDate() - 7);
 	for (var i = 0; i < cookieArray.length; i++) {
 		document.cookie = cookieArray[i] + "; expires=" + expiresDate.toUTCString();
