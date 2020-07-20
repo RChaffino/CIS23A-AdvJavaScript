80d098835638e23428aaee4c1ad5b24d692c98e3
RChaffino <67568390+RChaffino@users.noreply.github.com>
2020-07-20 02:07:36 -0700

    apikey placement

--- chapter11/script.js
+++ chapter11/script.js
@@ -53,7 +53,7 @@ function getWeather(evt) {
 	   httpRequest = getRequestObject();
    }
    httpRequest.abort();
-   httpRequest.open("get","solar.php?" + "lat=" + latitude + "&lon=" + longitude, true);
+   httpRequest.open("get","solar.php?" + "lat=" + latitude + "&lon=" + longitude + "&cnt=7&appid=3c023ae6f83867dd4549eb5a5237307e", true);
    httpRequest.send(null);
 }
 
--- chapter11/solar.php
+++ chapter11/solar.php
@@ -1,6 +1,6 @@
 <?php
-$WeatherSource = "https://api.openweathermap.org/data/2.5/forecast/daily?cnt=7&appid=3c023ae6f83867dd4549eb5a5237307e" . 
-$_GET["lat"] . "," . $_GET["lon"];
+$WeatherSource = "https://api.openweathermap.org/data/2.5/forecast/daily?" . 
+$_GET["lat"] . "," . $_GET["lon"] ;
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
