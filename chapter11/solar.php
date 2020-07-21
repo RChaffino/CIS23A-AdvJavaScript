6f3f45a7f2568b13e5540ccb7262c881ae282b5c
RChaffino <67568390+RChaffino@users.noreply.github.com>
2020-07-21 02:41:04 -0700

    Update solar.php

--- chapter11/solar.php
+++ chapter11/solar.php
@@ -1,5 +1,9 @@
 <?php
-$WeatherSource = "https://api.openweathermap.org/data/2.5/onecall?lat=" . $_GET["lat"] . "&lon=" . $_GET["lng"] . "&appid=3c023ae6f83867dd4549eb5a5237307e";
+$WeatherSource = "https://api.openweathermap.org/data/2.5/onecall?"
+	. "lat=" . $_GET["lat"] 
+	. "&lon=" . $_GET["lng"] 
+	. "&exclude=minutely,hourly" 
+	. "&appid=3c023ae6f83867dd4549eb5a5237307e";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
