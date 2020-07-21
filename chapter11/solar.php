1cb8cd19b4bda1c4c58e951c4a1c71cafb4b5449
RChaffino <67568390+RChaffino@users.noreply.github.com>
2020-07-20 22:15:31 -0700

    Update solar.php

--- chapter11/solar.php
+++ chapter11/solar.php
@@ -1,7 +1,5 @@
 <?php
-$WeatherSource = "https://api.openweathermap.org/data/2.5/forecast/daily?" . 
-"lat=" . $_GET["lat"] . "&lon=" . $_GET["lng"] .
-"&cnt=7&appid=3c023ae6f83867dd4549eb5a5237307e";
+$WeatherSource = "api.openweathermap.org/data/2.5/forecast/daily?lat=" . $_GET["lat"] . "&lon=" . $_GET["lng"] . "&cnt=16&appid=3c023ae6f83867dd4549eb5a5237307e";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
